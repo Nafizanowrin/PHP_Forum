03351e1ec70d55657b1e424d1166408df0e818cc
Nafizanowrin <115865205+Nafizanowrin@users.noreply.github.com>
2024-07-14 01:14:19 +0600

    Modified update.php

--- php_forms/update.php
+++ php_forms/update.php
@@ -18,10 +18,25 @@ $currentEmail = $_SESSION['email'];
 $sql = "UPDATE users SET username='$newUsername', email='$newEmail', password='$newPassword' WHERE email='$currentEmail'";
 if ($conn->query($sql) === TRUE) {
     $_SESSION['email'] = $newEmail;
-    echo "Update successful";
+    $message = "Update successful";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    $message = "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update Status</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Update Status</h2>
+        <p id="message"><?php echo $message; ?></p>
+        <a href="dashboard.php" class="button">Go to Dashboard</a>
+    </div>
+</body>
+</html>
