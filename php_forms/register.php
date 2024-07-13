f6af08ac8c833908acdd65e91f72e47cd651a88f
Nafizanowrin <115865205+Nafizanowrin@users.noreply.github.com>
2024-07-14 01:11:52 +0600

    Update register.php

--- php_forms/register.php
+++ php_forms/register.php
@@ -4,12 +4,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-// Insert data into the database
+
 $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
 if ($conn->query($sql) === TRUE) {
     echo "Registration successful";
